7566c6d1e5f1a6da268068a7a145b9d6e37877b6
raja9542 <rajasekhar1banda@gmail.com>
2023-11-16 11:50:59 +0530

    redis module with redis cluster change

--- main.tf
+++ main.tf
@@ -35,15 +35,19 @@ resource "aws_security_group" "elasticache" {
 }
 
 
-resource "aws_elasticache_replication_group" "elasticache" {
-  replication_group_id       = "${var.env}-elasticache" // any name we can give
-  description                = "example description"
-  node_type                  = var.node_type
-  port                       = 6379
-  automatic_failover_enabled = true
+resource "aws_elasticache_cluster" "elasticache" {
+  cluster_id           = "${var.env}-elasticache"
+  engine               = "redis"
+  node_type            = var.node_type
+  num_cache_nodes      = var.num_cache_nodes
+  engine_version       = var.engine_version
+  port                 = 6379
   subnet_group_name          = aws_elasticache_subnet_group.default.name
   security_group_ids         = [aws_security_group.elasticache.id]
 
-  num_node_groups            = var.num_node_groups
-  replicas_per_node_group    = var.replicas_per_node_group
+  tags = merge(
+    local.common_tags,
+    {Name = "${var.env}-elasticache-security-group"}
+  )
+
 }
